67c0d325a0e9ea6682eb23ed1a3d85b1896f726d
gabrielpastori <gabrielpastorifigueira@gmail.com>
2021-12-05 03:55:59 -0300

    update requirements

--- analysis_django/analysis_django/settings.py
+++ analysis_django/analysis_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ukkc_411l@no!u(&0(ht&-5=1+46kbxqm78hc^t5ywcfajayn&'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
